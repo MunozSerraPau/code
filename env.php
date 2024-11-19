0e2e6896059074fbd6596aa042c3ec4cfdd0d0de
Pau Muñoz Serra <munozserrap@gmail.com>
2024-11-19 19:45:26 +0100

    Credencials en el env.php per conectarme a la bd de la paguina web

--- env.php
+++ env.php
@@ -5,10 +5,10 @@
     define('DB_PASS', '');
 
     /*
-    define('DB_HOST', 'localhost');
-    define('DB_NAME', 'pt04_pau_munoz');
-    define('DB_USER', 'root');
-    define('DB_PASS', '');
+    define('DB_HOST', 'bbdd.paumunoz.cat');
+    define('DB_NAME', 'ddb237719');
+    define('DB_USER', 'ddb237719');
+    define('DB_PASS', 'P@ssw0rd');
     */
 
     define('BASE_PATH', __DIR__ );
