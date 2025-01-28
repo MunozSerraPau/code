90180e6e8280ec65e477cadbab87dca6cd48284f
Pau Muñoz Serra <munozserrap@gmail.com>
2025-01-28 16:12:11 +0100

    Rutas style correctes

--- index.php
+++ index.php
@@ -3,11 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>INDEX</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <link rel="stylesheet" href="./style/reset.css">
-    <link rel="stylesheet" href="./style/style.css">
+    <link rel="stylesheet" href="vista/style/style.css">
+    <title>INDEX</title>
 </head>
 <body>
     <?php if (session_status() === PHP_SESSION_NONE) { session_start(); } ?>
--- vistaGlobal/error/404.php
+++ vistaGlobal/error/404.php
@@ -5,8 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <link rel="stylesheet" href="../../style/reset.css">
-    <link rel="stylesheet" href="../../style/style.css">
+    <link rel="stylesheet" href="vista/style/style.css">
     <title>404</title>
 </head>
 <body>
